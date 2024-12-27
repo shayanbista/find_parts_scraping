7440b4594094a446be9e72332639dc83df5c0521
leapfrog <shayanbista94@gmail.com>
2024-12-27 15:02:52 +0545

    assignment add

--- main.py
+++ main.py
@@ -4,55 +4,22 @@ from bs4 import BeautifulSoup
 import requests
 import time
 from scraper.find_parts_scraping import PartsDetailScraper
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def main():
 
     url="https://www.findchips.com/detail/rr142-bb-nn/Carling-Technologies"
+    driver = webdriver.Chrome()
+    driver.get(url)
+    time.sleep(5)  
+    html = driver.page_source
+    # response = requests.get(url)
 
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
     scrape=PartsDetailScraper(soup)
     scrape.parse()
     print(scrape.parse()) 
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-    # response = requests.get(url)
-    # soup = BeautifulSoup(response.content, "html5lib")
-
-    # scrape = PartsDetailScraper(soup)
-    # print(scrape.parse())
-
 
 if __name__ == "__main__":
     main()
